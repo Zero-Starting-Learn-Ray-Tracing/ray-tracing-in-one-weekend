78fd7ec58253f3fe46aa188f314ca221d3a7ee8f
xiaomx32 <2116906041@qq.com>
2024-05-03 11:11:09 +0800

    ch-05

---
+++ src/camera.rs
@@ -0,0 +1,45 @@
+use nalgebra::Vector3;
+// use crate::ray::Ray;
+
+#[derive(Debug)]
+pub struct Camera {
+    pub center: Vector3<f32>,
+    pub pixel00_loc: Vector3<f32>,// location of pixel (0, 0)
+    pub pixel_delta_u: Vector3<f32>,
+    pub pixel_delta_v: Vector3<f32>
+}
+
+impl Camera {
+    pub const ASPECT_RATIO: f32 = 16.0 / 9.0;
+    pub const IMAGE_WIDTH: u32 = 960;
+    pub const IMAGE_HEIGHT: u32 = if Self::IMAGE_WIDTH as f32 / Self::ASPECT_RATIO < 1.0 {
+        1
+    } else {
+        (Self::IMAGE_WIDTH as f32 / Self::ASPECT_RATIO) as u32
+    };
+
+    pub fn new() -> Self {
+        let center = Vector3::new(0.0, 0.0, 0.0);
+        let focal_length = 1.0;
+        let viewport_height = 2.0;
+        let viewport_width = viewport_height * (Self::IMAGE_WIDTH as f32 / Self::IMAGE_HEIGHT as f32);
+        let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
+        let viewport_v = Vector3::new(0.0, viewport_height, 0.0);
+        let pixel_delta_u = viewport_u / Self::IMAGE_WIDTH as f32;
+        let pixel_delta_v = viewport_v / Self::IMAGE_HEIGHT as f32;
+
+        let viewport_upper_left =
+            center
+            - Vector3::new(0.0, 0.0, focal_length)
+            - viewport_u / 2.0
+            - viewport_v / 2.0;
+        let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        return Camera {
+            center,
+            pixel00_loc,
+            pixel_delta_u,
+            pixel_delta_v
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod ray;
+mod camera;
 mod hitable;
 mod sphere;
 
@@ -6,6 +7,7 @@ use std::f32;
 use std::io::{ stderr, Write };
 use nalgebra::Vector3;
 use crate::ray::Ray;
+use crate::camera::Camera;
 use crate::hitable::{ Hitable, HitableList };
 use crate::sphere::Sphere;
 
@@ -20,32 +22,9 @@ fn color(ray: &Ray, world: &HitableList) -> Vector3<f32> {
 }
 
 fn main() {
-    // image
-    const ASPECT_RATIO: f32 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u32 = 960;
-    const IMAGE_HEIGHT: u32 = if IMAGE_WIDTH as f32 / ASPECT_RATIO < 1.0 {
-        1
-    } else {
-        (IMAGE_WIDTH as f32 / ASPECT_RATIO) as u32
-    };
-
     // camera
-    let focal_length = 1.0;
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * (IMAGE_WIDTH as f32 / IMAGE_HEIGHT as f32);
-    let camera_center = Vector3::new(0.0, 0.0, 0.0);
-    let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vector3::new(0.0, -viewport_height, 0.0);
-
-    let pixel_delta_u = viewport_u / (IMAGE_WIDTH as f32);
-    let pixel_delta_v = viewport_v / IMAGE_HEIGHT as f32;
-
-    let viewport_upper_left =
-        camera_center
-        - Vector3::new(0.0, 0.0, focal_length)
-        - viewport_u / 2.0
-        - viewport_v / 2.0;
-    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    let camera = Camera::new();
+    eprint!("Camera: {:#?}", camera);
 
     // world
     let world = HitableList::new(vec![
@@ -53,17 +32,17 @@ fn main() {
         Box::new(Sphere::new(Vector3::new(0.0, -100.5, -1.0), 100.0))
     ]);
 
-    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
-    for j in 0..IMAGE_HEIGHT {
-        eprint!("\rScanlines remaining: {:3}", IMAGE_HEIGHT - j - 1);
+    println!("P3\n{} {}\n255", Camera::IMAGE_WIDTH, Camera::IMAGE_HEIGHT);
+    for j in (0..Camera::IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {:3}", Camera::IMAGE_HEIGHT - j);
         stderr().flush().unwrap();
-        for i in 0..IMAGE_WIDTH {
+        for i in 0..Camera::IMAGE_WIDTH {
             let pixel_center =
-                pixel00_loc
-                + (i as f32 * pixel_delta_u)
-                + (j as f32 * pixel_delta_v);
-            let ray_direction = pixel_center - camera_center;
-            let ray = Ray::new(camera_center, ray_direction);
+                camera.pixel00_loc
+                + (i as f32 * camera.pixel_delta_u)
+                + (j as f32 * camera.pixel_delta_v);
+            let ray_direction = pixel_center - camera.center;
+            let ray = Ray::new(camera.center, ray_direction);
             let col = color(&ray, &world);
             let ir = (255.99 * col[0]) as i32;
             let ig = (255.99 * col[1]) as i32;
